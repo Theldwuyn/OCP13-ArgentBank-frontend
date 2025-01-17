b15f5fb94e2f712525ef508ac3e9c4bc6497cdcf
Guillaume <guillaumeargensse@hotmail.fr>
2025-01-17 15:07:23 +0100

    feat: remove unused selector

--- src/common/selector.js
+++ src/common/selector.js
@@ -1,11 +1,3 @@
-export function isUserAuthentified(state) {
-  return state.user.isAuthentified;
-}
-
 export function selectFirstName(state) {
-  return state.user.firstName;
-}
-
-export function selectBankFeatures(state) {
-  return state.bank.features;
+  return state.auth.userInfos.firstName;
 }
