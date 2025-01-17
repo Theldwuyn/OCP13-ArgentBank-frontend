e96d48f110a029240f51cc629299d7147045f896
Guillaume <guillaumeargensse@hotmail.fr>
2025-01-17 15:09:01 +0100

    feat: add new route to router for Dashboard page, add Dashboard component with placeholder content

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,13 +3,15 @@ import Home from '../pages/Home';
 import Header from '../common/components/Header';
 import SignIn from '../pages/SignIn';
 import Footer from '../common/components/Footer';
+import Dashboard from '../pages/Dashboard';
 
 function AppRoutes() {
   return (
-    <div>
+    <div className="main-div">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
